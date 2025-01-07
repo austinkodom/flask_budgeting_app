dd5fdd97135591d1833f9aa3290b3f4a5fac4fa6
Austin Odom <aodom4@una.edu>
2025-01-07 00:41:27 -0600

    a

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from website import create_app
-from waitress import serve
 
 app = create_app()
 mode = 'dev'
@@ -8,6 +7,3 @@ if __name__ == '__main__':
     if mode == 'dev':
         # Testing Environment
         app.run(debug=True)
-    else:
-        # Production Environment
-        serve(app, host='0.0.0.0', port='50100', threads=2, url_prefix='/my_budget')
