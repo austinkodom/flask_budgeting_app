a8b55e1c3eefaf07d58d919bc2216fd8b0f66922
austinkodom <aodom4@una.edu>
2025-01-22 22:40:22 -0600

    main to wsgi

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from website import create_app
 
 app = create_app()
-mode = 'dev'
 
 if __name__ == '__main__':
-    if mode == 'dev':
-        # Testing Environment
-        app.run(debug=True)
+    app.run()
